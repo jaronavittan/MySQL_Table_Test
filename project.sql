74d6e6e97b3fddfac02b4d78c16e79e15f416891
Jaron Avittan <96601217+jaronavittan@users.noreply.github.com>
2023-07-26 11:56:58 -0500

    Create project.sql

---
+++ project.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Projects (
+  CustomerID int,
+  ProjectID int,
+  TypeOfProject varchar,
+  StartDate int,
+  EndDate int,
+  Hours int,
+  BillingAmount int
+);
+
+INSERT INTO Projects ();
